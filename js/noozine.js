e436b54b33b83c710bb0087ee6aefe114e1ed32a
David Walther Birk <david@scripler.com>
2016-06-21 23:02:43 +0200

    Prototyping intro

--- js/noozine.js
+++ js/noozine.js
@@ -4,11 +4,7 @@
   var app = angular.module('noozine', ['ngMaterial']);
   app.controller('zineController', ['$scope', function($scope){
     $scope.year = 2121;
-    setTimeout(function(){$scope.year++; console.log($scope.year);}, 1000);
 
-    $scope.add = function() {
-      $scope.year++;
-    };
 
 
 
